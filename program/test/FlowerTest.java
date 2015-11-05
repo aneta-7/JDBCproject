b3c29c1dfa28ec4c0a5eaecf574d276e97c25fe3
aneta-stypa <aneta.stypa7@wp.pl>
2015-11-05 20:39:22 +0100

    end commit

--- program/test/FlowerTest.java
+++ program/test/FlowerTest.java
@@ -1,12 +1,11 @@
-package test;
+package aneta.aneta;
 
 import static org.junit.Assert.*;
 
 import java.util.List;
 import org.junit.Test;
-
-import main.Flower;
-import main.FlowerManager;
+import org.hsqldb.auth.JaasAuthBean.UPCallbackHandler;
+import aneta.aneta.*;
 
 public class FlowerTest {
 
@@ -16,8 +15,9 @@ FlowerManager flowerManager = new FlowerManager();
 	
 	private final static long ID_1 = 1;
 	private final static String NAME_1 = "roza";
-	private final static String NAME_2 = "tulipan";
+	private final static String NEW_NAME = "tulipan";
 	private final static String PRICE_1 = "4";
+	private final static String NEW_PRICE = "3";
 	
 	@Test
 	public void checkConnection(){
@@ -41,25 +41,40 @@ FlowerManager flowerManager = new FlowerManager();
 	
 	@Test
 	public void checkUpdate(){
-		Flower flower = new Flower(NAME_2, ID_1);	
-			checkAdding();
-		assertEquals(1, flowerManager.updateFlower(flower));	
+		Flower flower = new Flower(NAME_1, PRICE_1);	
+	
+		flowerManager.clearFlower();
+		assertEquals(1, flowerManager.addFlower(flower));
+		
+		List<Flower> flowers = flowerManager.getAllFlower();
+		Flower flowerRetrieved = flowers.get(0);
+		
+		flowerRetrieved.setFlowerName(NEW_NAME);
+		flowerRetrieved.setFlowerPrice(NEW_PRICE);
+		
+		assertEquals(1, flowerManager.updateFlower(flowerRetrieved));
+		
+		List<Flower> flowers2 = flowerManager.getAllFlower();
+		Flower flowerRetrieved2 = flowers2.get(0);
+		
+		assertEquals(NEW_NAME, flowerRetrieved2.getFlowerName());
+		assertEquals(NEW_PRICE, flowerRetrieved2.getFlowerPrice());
 	}
 	
 	@Test
 	public void checkDelete(){
+		Flower flower = new Flower(NAME_1, PRICE_1);
+		
+		flowerManager.clearFlower();
+		assertEquals(1,flowerManager.addFlower(flower));
+		
+		List<Flower> flowers = flowerManager.getAllFlower();
+		Flower flowerRetrieved = flowers.get(0);
+		
+		assertEquals(1, flowerManager.deleteFlower(flowerRetrieved));
 		
 	}
-	
-	@Test
-	public void checkSelect(){
 		
-	}
-	
-	
-	
-	
-	
 	
 	
 }
