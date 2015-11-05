a3cfe2ee56cdb9b5a2890b3ae6d3afefb44bddf2
aneta-stypa <aneta.stypa7@wp.pl>
2015-11-05 20:40:01 +0100

    end commit

--- program/test/posredniczacaTest.java
+++ program/test/posredniczacaTest.java
@@ -1,38 +1,31 @@
-package test;
+package aneta.aneta;
 
 import static org.junit.Assert.*;
 
 import java.util.List;
 
 import org.junit.Test;
-
-import main.posredniczaca;
-import main.FlowerManager;
-import main.Bouqet;
-import main.BouqetManager;
-import main.Flower;
+import org.hsqldb.auth.JaasAuthBean.UPCallbackHandler;
+import aneta.aneta.*;
 
 public class posredniczacaTest {
 
 	
 posredniczaca posred = new posredniczaca();
+
 FlowerManager flowerManager = new FlowerManager();
 BouqetManager bouqetManager = new BouqetManager();
+BouqetFlowerManager bouqetFlowerManager = new BouqetFlowerManager();
 	
 
 private final static String NAME_1 = "mieszanka";
+private final static String NEW_NAME = "wiazanka";
 private final static String NAME_2 = "tulipan";
 private final static String PRICE_1 = "4";
 
 	@Test
-	//test pobierz x nalezacych do y
-	public void test1() {
-	
-	}
-
-	@Test
 	//test dodanie do tabeli y
-	public void test2(){
+	public void checkAddY(){
 		Bouqet bouqet = new Bouqet(NAME_1);
 		
 		assertEquals(1,bouqetManager.addBouqet(bouqet));
@@ -41,23 +34,48 @@ private final static String PRICE_1 = "4";
 	
 	@Test
 	//test pobranie wszystkich y
-	public void test3(){
+	public void checkSelectY(){
+			
+		Bouqet bouqet = new Bouqet(NAME_1);
+		
+		bouqetManager.clearBouqet();
+		assertEquals(1,bouqetManager.addBouqet(bouqet));
+		
 		List<Bouqet> bouqets = bouqetManager.getAllBouqet();
 		Bouqet bouqetRetrieved = bouqets.get(0);
 		
+		bouqetRetrieved.setBouqetName(NAME_1);
 		assertEquals(NAME_1, bouqetRetrieved.getBouqetName());
+		
+		//assertEquals(2,bouqets.size());
 	}
 	
 	@Test 
 	//test przypisanie x do y
-	public void test4(){
+	public void checkXtoY(){
+		Flower flower = new Flower(NAME_2, PRICE_1);
 		
+		flowerManager.clearFlower();
+		assertEquals(1, flowerManager.addFlower(flower));
+		
+		List<Flower> flowers = flowerManager.getAllFlower();
+		Flower flowerRetrieved = flowers.get(0);
+		
+		Bouqet bouqet = new Bouqet(NAME_1);
+		
+		bouqetManager.clearBouqet();
+		assertEquals(1,bouqetManager.addBouqet(bouqet));
+		
+		List<Bouqet> bouqets = bouqetManager.getAllBouqet();
+		Bouqet bouqetRetrieved = bouqets.get(0);
+
+		assertEquals(1, bouqetFlowerManager.addFlowerBouqet(flowerRetrieved,bouqetRetrieved));
 		
 	}
 	
 	@Test
 	//test dodanie do tabeli x
-	public void test5(){
+	public void checkAddX(){
 		Flower flower = new Flower(NAME_2, PRICE_1);
 		
 		flowerManager.clearFlower();
